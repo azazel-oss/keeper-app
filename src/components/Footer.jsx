75c26934bf544d5e31cf2dd58e2ca329a439382c
azazel-oss <65448804+azazel-oss@users.noreply.github.com>
2021-04-14 23:37:37 +0530

    sharpen the stage 1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function Footer() {
     const today = new Date();
     const year = today.getFullYear();
-    return <footer>Copyright {year}</footer>;
+    return <footer><p>
+        Copyright © {year}
+    </p></footer>;
 }
 
 export default Footer;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Note() {
     return <div className="note"><h1>This is the title</h1>
-    <p>This is note content</p></div>;
+        <p>This is note content</p></div>;
 }
 
 export default Note;
