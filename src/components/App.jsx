7abf21594faa4ade31c707803de8f360a968943c
azazel-oss <65448804+azazel-oss@users.noreply.github.com>
2021-04-15 02:36:51 +0530

    completed stage 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
-
+import notes from '../notes';
 
 
 function App() {
-    return <div><Header /> <Note /> <Footer /></div>;
+    return <div><Header /> {notes.map(currentNote => <Note 
+        key={currentNote.key}
+        title={currentNote.title}
+        content={currentNote.content}
+    />)} <Footer /></div>;
 }
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Note() {
-    return <div className="note"><h1>This is the title</h1>
-        <p>This is note content</p></div>;
+function Note(props) {
+    return <div className="note"><h1>{props.title}</h1>
+        <p>{props.content}</p></div>;
 }
 
 export default Note;
